a37ee88fc8bfa653a3628cc854811a8e92b93bbf
CLux <cplux7@gmail.com>
2019-06-02 14:23:50 -0400

    cleared out all errors to try to run first test

--- UCHProject.Tests/DoctorTests.cs
+++ UCHProject.Tests/DoctorTests.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using Xunit;
-using UCHProject
+using UCHProject;
 
 namespace UCHProject.Tests
 {
-    class DoctorTests
+    public class DoctorTests
     {
         [Fact]
         public void Doctor_constructor_should_Use_employee_base_constructor_and_then_overwrite_via_class_constructor()
         {
             //arange
+            string name = "johnsmith";
+            int employeeNumber = 001;
+            string specialty = "Neuro";
 
             //act
+            Doctor sut = new Doctor(name, employeeNumber, specialty);
 
             //assert
-            Assert.Equal("Fail", EmployeeType);
+            Assert.Equal(000, sut.EmployeeNumber);
+            //Assert.Equal("Fail", sut.EmployeeType);
+            //Assert.Equal(0, sut.Salary);
+            //Assert.True(sut.BeenPaid);
+            //Assert.Equal("fail", sut.Specialty);
+            //Assert.Equal("johnsmith", sut.Name);
 
         }
 
